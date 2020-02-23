6fc8147c10b694c903427d260ef62127401a88d9
Duncan Taylor <Duncant@gmail.com>
2020-02-23 21:35:42 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Amplify, * as AmplifyModules from "aws-amplify";
+import { AmplifyPlugin } from "aws-amplify-vue";
+import awsconfig from "./aws-exports";
+import axios from "axios";
+
+Amplify.configure(awsconfig);
+
+Vue.use(AmplifyPlugin, AmplifyModules);
 
 Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
 
 new Vue({
   router,
